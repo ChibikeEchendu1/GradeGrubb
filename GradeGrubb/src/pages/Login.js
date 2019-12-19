9242b1b983e2dda25b75604215e450abcad39902
ChibikeEchendu1 <32308443+ChibikeEchendu1@users.noreply.github.com>
2019-12-19 12:44:04 +0100

    Nicer Login Page

--- GradeGrubb/src/pages/Login.js
+++ GradeGrubb/src/pages/Login.js
@@ -4,55 +4,38 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import LoginForm from '.././components/LoginForm';
-import {Provider} from 'react-redux';
-import{createStore, applyMiddleware} from 'redux';
-import ReduxThunk from 'redux-thunk'
-import reducers from '../reducers'
-import {
-  Platform,
-  StyleSheet,
-  BackHandler,
-  Text,
-  View,
-  TouchableWithoutFeedback,
-  KeyboardAvoidingView,
-  SafeAreaView,
-  Keyboard
-} from 'react-native';
-
-
-
-
+import React, { Component } from "react";
+import LoginForm from ".././components/LoginForm";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
+import reducers from "../reducers";
+import { StyleSheet, BackHandler } from "react-native";
 
 export default class Login extends Component {
-  static navigationOptions = { header: null };
+	static navigationOptions = { header: null };
 
-  componentWillMount(){ 
-    BackHandler.addEventListener('hardwareBackPress',function(){
-      return true;
-    });
-  }
+	componentWillMount() {
+		BackHandler.addEventListener("hardwareBackPress", function() {
+			return true;
+		});
+	}
 
-  render() {
-    const {navigation} = this.props
-    return (
-    <Provider store={createStore(reducers, {},applyMiddleware(ReduxThunk))}>
-        <LoginForm navigation={navigation}/>
-     </Provider>
-    );
-  }
-} 
+	render() {
+		const { navigation } = this.props;
+		return (
+			<Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+				<LoginForm navigation={navigation} />
+			</Provider>
+		);
+	}
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F4F2F3',
-  },
- 
-  
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "#F4F2F3"
+	}
 });
-
